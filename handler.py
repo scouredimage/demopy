8e7ce5a2a53a0b906da8dd2610dfbd4533275b5f
Sandesh Devaraju <scouredimage@gmail.com>
2020-02-05 12:50:50 -0800

    init

---
+++ handler.py
@@ -0,0 +1,16 @@
+import json
+
+def hello(event, context):
+    context.serverless_sdk.set_endpoint('/german/gutentag')
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
